834fa75fb35567cc0ab523bef0af19bb2b3a2bf8
Catriona Docherty <cdocherty@wearearchitect.com>
2016-01-28 14:36:38 +0000

    chore(): first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+var io = require('socket.io'),
+    connect = require('connect'),
+    express = require('express');
+
+
+var app = connect().use(connect.static('public')).listen(3000);
+var chat_room = io.listen(app);
+var usernames = {};
+
+
+chat_room.sockets.on('connection', function(socket) {
+
+    socket.on('adduser', function(username) {
+        socket.username = username;
+        usernames[username] = username;
+
+        socket.emit('updatechat', 'SERVER', 'Welcome to the chat room!');
+        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        chat_room.sockets.emit('updateusers', usernames);
+    });
+
+    socket.on('disconnect', function() {
+        delete usernames[socket.username];
+        chat_room.sockets.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
+
+    socket.on('sendchat', function(data) {
+        chat_room.sockets.emit('updatechat', socket.username, data);
+    });
+});
