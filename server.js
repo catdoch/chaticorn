2e7d40d9e6a10af7dab8e4fce8e830951bdfe609
Catriona Docherty <cdocherty@wearearchitect.com>
2016-01-29 13:52:32 +0000

    chore(): add js folder

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var io = require('socket.io'),
     port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + "/public"));
-app.use(express.static(__dirname + "/javascript"));
 
 var chat_room = io.listen(server);
 
