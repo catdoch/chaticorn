0107f7263635f1256cb7f801ee590124ad040f65
Catriona Docherty <cdocherty@wearearchitect.com>
2016-01-29 12:11:47 +0000

    chore(): try heroku web sockets

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
+var usernames = {};
 
 app.use(express.static(__dirname + "/public"));
 
@@ -18,49 +19,25 @@ console.log("http server listening on %d", port);
 var wss = new WebSocketServer({
     server: server
 });
-console.log("websocket server created");
 
-wss.on("connection", function(ws) {
-    var id = setInterval(function() {
-        ws.send(JSON.stringify(new Date()), function() {});
-    }, 1000);
+wss.on('connection', function(socket) {
 
-    console.log("websocket connection open");
+    socket.on('adduser', function(username) {
+        socket.username = username;
+        usernames[username] = username;
 
-    ws.on("close", function() {
-        console.log("websocket connection close");
-        clearInterval(id);
+        socket.emit('updatechat', 'SERVER', 'Welcome to the chat room!');
+        socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
+        wss.emit('updateusers', usernames);
+    });
+
+    socket.on('disconnect', function() {
+        delete usernames[socket.username];
+        wss.emit('updateusers', usernames);
+        socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
+    });
+
+    socket.on('sendchat', function(data) {
+        wss.emit('updatechat', socket.username, data);
     });
 });
-
-
-
-// var port = process.env.PORT || 8080;
-
-// var app = connect().use(connect.static('public')).listen(process.env.PORT || 3000);
-
-// var chat_room = io.listen(app);
-// var usernames = {};
-
-
-// chat_room.sockets.on('connection', function(socket) {
-
-//     socket.on('adduser', function(username) {
-//         socket.username = username;
-//         usernames[username] = username;
-
-//         socket.emit('updatechat', 'SERVER', 'Welcome to the chat room!');
-//         socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-//         chat_room.sockets.emit('updateusers', usernames);
-//     });
-
-//     socket.on('disconnect', function() {
-//         delete usernames[socket.username];
-//         chat_room.sockets.emit('updateusers', usernames);
-//         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
-//     });
-
-//     socket.on('sendchat', function(data) {
-//         chat_room.sockets.emit('updatechat', socket.username, data);
-//     });
-// });
