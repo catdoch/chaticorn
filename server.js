f6e929c90ccaccee961212bc4c28783d541c3d71
Catriona Docherty <cdocherty@wearearchitect.com>
2016-01-29 12:06:43 +0000

    chore(): try heroku web sockets

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
 
-app.use(express.static(__dirname + "/"));
+app.use(express.static(__dirname + "/public"));
 
 var server = http.createServer(app);
 server.listen(port);
