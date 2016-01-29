6ac3514dd84d2a9be64a598be9f85f1a3ac2d6eb
Catriona Docherty <cdocherty@wearearchitect.com>
2016-01-29 12:43:56 +0000

    chore(): revert

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
-// var io = require('socket.io'),
-//     connect = require('connect');
+var io = require('socket.io'),
+    connect = require('connect');
 
+var port = process.env.PORT || 8080;
 
-var WebSocketServer = require("ws").Server;
-var http = require("http");
-var express = require("express");
-var app = express();
-var port = process.env.PORT || 5000;
+var chat_room = io.listen();
 var usernames = {};
 
-app.use(express.static(__dirname + "/public"));
 
-var server = http.createServer(app);
-server.listen(port);
-
-console.log("http server listening on %d", port);
-
-var wss = new WebSocketServer({
-    server: server
-});
-
-wss.on('connection', function(socket) {
+chat_room.sockets.on('connection', function(socket) {
 
     socket.on('adduser', function(username) {
         socket.username = username;
@@ -28,16 +15,16 @@ wss.on('connection', function(socket) {
 
         socket.emit('updatechat', 'SERVER', 'Welcome to the chat room!');
         socket.broadcast.emit('updatechat', 'SERVER', username + ' has connected');
-        wss.emit('updateusers', usernames);
+        chat_room.sockets.emit('updateusers', usernames);
     });
 
     socket.on('disconnect', function() {
         delete usernames[socket.username];
-        wss.emit('updateusers', usernames);
+        chat_room.sockets.emit('updateusers', usernames);
         socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' has disconnected');
     });
 
     socket.on('sendchat', function(data) {
-        wss.emit('updatechat', socket.username, data);
+        chat_room.sockets.emit('updatechat', socket.username, data);
     });
 });
